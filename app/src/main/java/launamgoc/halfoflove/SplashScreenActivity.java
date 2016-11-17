073be2030395e6a3b595b1ecf7bc85d83c77d13b
Ngọc Hưng <ngochung1243@gmail.com>
2016-11-17 21:10:46 +0700

    update for first time: Firebase library + 3 views: splash, login, register

--- app/src/main/java/launamgoc/halfoflove/SplashScreenActivity.java
+++ app/src/main/java/launamgoc/halfoflove/SplashScreenActivity.java
@@ -3,11 +3,11 @@ package launamgoc.halfoflove;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main3Activity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_splashscreen);
     }
 }
