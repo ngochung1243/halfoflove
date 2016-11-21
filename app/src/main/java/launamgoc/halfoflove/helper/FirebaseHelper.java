61878eb7c42456f996827dfb4d0b905e482fac34
Ngọc Hưng <ngochung1243@gmail.com>
2016-11-21 16:57:17 +0700

    - Merge tree: phase 1.5 and phase 1

---
+++ app/src/main/java/launamgoc/halfoflove/helper/FirebaseHelper.java
@@ -0,0 +1,154 @@
+package launamgoc.halfoflove.helper;
+
+import launamgoc.halfoflove.model.User;
+
+/**
+ * Created by Admin on 11/21/2016.
+ */
+
+public class FirebaseHelper {
+
+    /**
+     * Create new User with email and password
+     * @param email
+     * @param password
+     * @return
+     */
+    static public boolean createNewUser(String email, String password){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Login user with email and password
+     * @param email
+     * @param password
+     * @return
+     */
+    static public User loginWithUser(String email, String password){
+
+        // change return when operate code
+        return null;
+    }
+
+    /**
+     * Login user when login social (add necessary parameter)
+     * @return
+     */
+    static public User loginWithSocial(){
+
+        // change return when operate code
+        return null;
+    }
+
+    /**
+     * Renew Password when User forget (add necessary parameter)
+     * @return
+     */
+    static public boolean renewPassword(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Change Infomation of User (add necessary parameter)
+     * @return
+     */
+    static public boolean changeInfoOfUser(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Find an User (add necessary parameter)
+     * @return
+     */
+    static public User findUser(){
+
+        // change return when operate code
+        return null;
+    }
+
+    /**
+     * Upload an Image (add necessary parameter)
+     * @return
+     */
+    static public String uploadImage(){
+
+        // change return when operate code
+        return null;
+    }
+
+    /**
+     * Adding an Event when User setup (add necessary parameter)
+     * @return
+     */
+    static public boolean addEventWithUser(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Addind a Relationship between 2 Users (add necessary parameter)
+     * @return
+     */
+    static public boolean addRelationship(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Removing a Relationship between 2 Users (add necessary parameter)
+     * @return
+     */
+    static public boolean removeRelationship(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Adding follow when an User want to follow another user (add necessary parameter)
+     * @return
+     */
+    static public boolean addFollow(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Remove follow when an User don't want to follow another user anymore (add necessary parameter)
+     * @return
+     */
+    static public boolean removeFollow(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Adding New Feed when User adds an event (add necessary parameter)
+     * @return
+     */
+    static public boolean addNewFeed(){
+
+        // change return when operate code
+        return true;
+    }
+
+    /**
+     * Chat with another User (add necessary parameter)
+     * @return
+     */
+    static public boolean chatToUser(){
+
+        // change return when operate code
+        return true;
+    }
+}
