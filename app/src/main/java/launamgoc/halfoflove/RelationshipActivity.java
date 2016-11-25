7e948301b8b8d54bdf994963a6d6808ba2e0e712
DESKTOP-J2R4JMB\Huy <nguyenlinhvuhuy@gmail,com>
2016-11-25 09:54:17 +0700

    Update: Edit UI relationship

--- app/src/main/java/launamgoc/halfoflove/RelationshipActivity.java
+++ app/src/main/java/launamgoc/halfoflove/RelationshipActivity.java
@@ -9,36 +9,123 @@ import android.widget.Spinner;
 
 public class RelationshipActivity extends AppCompatActivity {
 
-    String arr[]={
+    String spnRelationship[]={
             "Single",
             "In a relationship",
             "Divorced"};
+
+    String spnTime[]={
+            "Day",
+            "Month",
+            "Year",
+            "Forever"
+    };
+
+    String spnYear[]={
+            "Year",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016"
+    };
+
+    String spnMonth[]={
+            "Month",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12"
+    };
+
+    String spnDay[]={
+            "Day", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15",
+            "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+            "30", "31"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relationship);
-        Spinner spin=(Spinner) findViewById(R.id.spnRelatioship);
-
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>
+        Spinner spinRelationship=(Spinner) findViewById(R.id.spnRelatioship);
+        Spinner spinTime =  (Spinner) findViewById(R.id.spnTime);
+        Spinner spinYear = (Spinner) findViewById(R.id.spnYear);
+        Spinner spinMonth = (Spinner) findViewById(R.id.spnMonth);
+        Spinner spinDay  = (Spinner) findViewById(R.id.spnDay);
+        ArrayAdapter<String> adapterRelationship=new ArrayAdapter<String>
                 (
                         this,
                         android.R.layout.simple_spinner_item,
-                        arr
+                        spnRelationship
                 );
-        adapter.setDropDownViewResource
+        adapterRelationship.setDropDownViewResource
                 (android.R.layout.simple_list_item_single_choice);
-        spin.setAdapter(adapter);
-        spin.setOnItemSelectedListener(new MyProcessEvent());
+        spinRelationship.setAdapter(adapterRelationship);
+        spinRelationship.setOnItemSelectedListener(new MyProcessEvent());
+
+        ArrayAdapter<String> adapterTime=new ArrayAdapter<String>
+                (
+                        this,
+                        android.R.layout.simple_spinner_item,
+                        spnTime
+                );
+        adapterTime.setDropDownViewResource
+                (android.R.layout.simple_list_item_single_choice);
+        spinTime.setAdapter(adapterTime);
+        spinTime.setOnItemSelectedListener(new MyProcessEvent());
+
+        ArrayAdapter<String> adapterYear=new ArrayAdapter<String>
+                (
+                        this,
+                        android.R.layout.simple_spinner_item,
+                        spnYear
+                );
+        adapterYear.setDropDownViewResource
+                (android.R.layout.simple_list_item_single_choice);
+        spinYear.setAdapter(adapterYear);
+        spinYear.setOnItemSelectedListener(new MyProcessEvent());
+
+        ArrayAdapter<String> adapterMonth=new ArrayAdapter<String>
+                (
+                        this,
+                        android.R.layout.simple_spinner_item,
+                        spnMonth
+                );
+        adapterMonth.setDropDownViewResource
+                (android.R.layout.simple_list_item_single_choice);
+        spinMonth.setAdapter(adapterMonth);
+        spinMonth.setOnItemSelectedListener(new MyProcessEvent());
+
+        ArrayAdapter<String> adapterDay=new ArrayAdapter<String>
+                (
+                        this,
+                        android.R.layout.simple_spinner_item,
+                        spnDay
+                );
+        adapterDay.setDropDownViewResource
+                (android.R.layout.simple_list_item_single_choice);
+        spinDay.setAdapter(adapterDay);
+        spinDay.setOnItemSelectedListener(new MyProcessEvent());
     }
 
     private class MyProcessEvent implements
             AdapterView.OnItemSelectedListener
     {
-        public void onItemSelected(AdapterView<?> arg0,
-                                   View arg1,
-                                   int arg2,
-                                   long arg3) {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
         }
+
         public void onNothingSelected(AdapterView<?> arg0) {
         }
     }
