9ce125debc2ac9eb16e7f0a1120b8097fbf2ce5c
Ngọc Hưng <ngochung1243@gmail.com>
2017-01-18 16:34:23 +0700

    update: + Fix login activity

--- app/src/main/java/launamgoc/halfoflove/activity/LoginActivity.java
+++ app/src/main/java/launamgoc/halfoflove/activity/LoginActivity.java
@@ -81,8 +81,60 @@ public class LoginActivity extends AppCompatActivity implements FirebaseHelper.F
 
         callbackManager = CallbackManager.Factory.create();
 
+        FirebaseHelper.loginDelegate = this;
+
 //        MyBundle.mUserBusiness.logout();
 
+        MapController();
+
+        setListener();
+
+        setSocialListener();
+
+        setProgressDialog();
+    }
+
+    private void setListener(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+                if (email == null || email == "" || password == null || password == ""){
+                    Toast.makeText(LoginActivity.this, "Please input all fields!!!", Toast.LENGTH_SHORT).show();
+                }
+
+                FirebaseHelper.loginWithUser(email,password);
+                progressDialog.show();
+//                TestGetToken();
+
+                //TestSendPostAPI();
+            }
+        });
+
+        btnSignInFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginWithFb();
+            }
+        });
+
+        btnSignInGG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginWithGG();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRegisterActivity();
+            }
+        });
+    }
+
+    private void setSocialListener(){
         // Callback registration
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -90,6 +142,7 @@ public class LoginActivity extends AppCompatActivity implements FirebaseHelper.F
                 // App code
                 Log.d("Facebook", "facebook:onSuccess:" + loginResult);
                 AuthCredential credential = FacebookAuthProvider.getCredential(loginResult.getAccessToken().getToken());
+                progressDialog.show();
                 FirebaseHelper.loginWithSocial(credential);
             }
 
@@ -121,93 +174,11 @@ public class LoginActivity extends AppCompatActivity implements FirebaseHelper.F
                 })
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
-        MapController();
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = edtEmail.getText().toString();
-                String password = edtPassword.getText().toString();
-                if (email == null || email == "" || password == null || password == ""){
-                    Toast.makeText(LoginActivity.this, "Please input all fields!!!", Toast.LENGTH_SHORT).show();
-                }
-
-                FirebaseHelper.loginWithUser(email,password);
-                progressDialog = new ProgressDialog(LoginActivity.this);
-                progressDialog.setMessage("Loading");
-                progressDialog.show();
-//                TestGetToken();
-
-                //TestSendPostAPI();
-            }
-        });
-        FirebaseHelper.loginDelegate = this;
-        btnSignInFb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loginWithFb();
-            }
-        });
-
-        btnSignInGG.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loginWithGG();
-            }
-        });
-
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startRegisterActivity();
-            }
-        });
     }
 
-    private void TestGetToken(){
-        String token = FirebaseHelper.getToken();
-        Toast.makeText(LoginActivity.this, "Token: ["+token+"]", Toast.LENGTH_SHORT).show();
-        Log.d("FirebaseToken","Token: ["+token+"]");
-    }
-
-    private void TestSendPostAPI(){
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://fcm.googleapis.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        FirebaseAPIHelper apiHelper = retrofit.create(FirebaseAPIHelper.class);
-        Message testMessage = new Message();
-        Map<String, String> data = new HashMap<>();
-        data.put("userID", "123456789");
-        data.put("kind", "message");
-        data.put("message", "hung dep trai");
-
-        testMessage.setData(data);
-        testMessage.setTo("chtmjx4Hiuk:APA91bGxdHe5NDhmfGRl-TaBq3u2bKsNuqTaRYTn_PIUnhxCvdte-7Cjl-8Ed9h9GYZuL-whGabzsQAEBtpv4D_QfTiaM9oflD9T4-KCs8Ym_NPoObkAS7KNhKz4uKVpgtPBhGI3l5aP");
-        Gson gson = new Gson();
-        String jsonString = gson.toJson(testMessage);
-        try {
-            JSONObject json = new JSONObject(jsonString);
-            Log.d("json", jsonString);
-
-            String test = "{to : chtmjx4Hiuk:APA91bGxdHe5NDhmfGRl-TaBq3u2bKsNuqTaRYTn_PIUnhxCvdte-7Cjl-8Ed9h9GYZuL-whGabzsQAEBtpv4D_QfTiaM9oflD9T4-KCs8Ym_NPoObkAS7KNhKz4uKVpgtPBhGI3l5aP}";
-            Call<MessageResponse> call =apiHelper.sendMessage(testMessage);
-            call.enqueue(new Callback<MessageResponse>() {
-                @Override
-                public void onResponse(Response<MessageResponse> response, Retrofit retrofit) {
-                    Log.d("Respone", response.toString());
-                }
-
-                @Override
-                public void onFailure(Throwable t) {
-                    int a = 0;
-                }
-            });
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
+    private void setProgressDialog(){
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setMessage("Loading...");
     }
 
     @Override
@@ -253,38 +224,17 @@ public class LoginActivity extends AppCompatActivity implements FirebaseHelper.F
         startActivity(intent);
     }
 
-    private void testChatMessage(){
-        Intent intent = new Intent(this, ChatActivity.class);
-        startActivity(intent);
-    }
-
-    private void testChatList(){
-        Intent intent = new Intent(this, ChatListTabActivity.class);
-        startActivity(intent);
-    }
-
     @Override
     public void onLoginSuccess(final User user) {
         hd.post(new Runnable() {
             @Override
             public void run() {
                 MyBundle.mUserBusiness.mUser = user;
-//                MyBundle.mUserBusiness.load(new UserBusiness.UserBusinessListener() {
-//                    @Override
-//                    public void onComplete(UserBusiness.UserBusinessResult result) {
-//                        if (result == UserBusiness.UserBusinessResult.SUCCESS){
-//                            MyBundle.pUserBusiness.mUser = MyBundle.mUserBusiness.pUser;
-//                            startMainActivity();
-//                        }
-//                    }
-//                });
                 if(progressDialog != null) {
                     progressDialog.dismiss();
                 }
                 MyBundle.mUserBusiness.getToken();
                 startMainActivity();
-//                testChatMessage();
-//                testChatList();
             }
         });
     }
@@ -297,6 +247,8 @@ public class LoginActivity extends AppCompatActivity implements FirebaseHelper.F
         if (requestCode == RC_GG_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()) {
+                progressDialog.show();
+
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = result.getSignInAccount();
                 AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
